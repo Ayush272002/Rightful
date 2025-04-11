749d279c7db065036278f7ea51a5b8aa603aaa97
Ayush Acharjya <ayushacharjya@gmail.com>
2025-04-11 17:16:10 +0100

    add wallet connecter

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,6 @@
 import { Button } from "@/components/ui/button";
+import WalletConnect from "@/components/WalletConnect";
 
 export default function Home() {
-  return (
-    <Button>
-      Hello world
-    </Button>
-  );
+  return <WalletConnect />;
 }
